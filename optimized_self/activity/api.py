816da9691fd9b0e2320f9c9454d4593b6d6eaf39
Cory Perkins <link2cory@gmail.com>
2019-07-21 15:40:52 -0700

    added a time_start and time_end field

--- optimized_self/activity/api.py
+++ optimized_self/activity/api.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, fields, marshal_with
 
 from optimized_self import db 
 from .post_parser import parse_args
@@ -7,13 +7,19 @@ from .post_parser import parse_args
 bp = Blueprint('activity_api', __name__, url_prefix='/api')
 api = Api(bp)
 
+activity_response_fields = {
+    'name': fields.String,
+    'time_start': fields.DateTime(),
+    'time_end': fields.DateTime()
+}
+
 class Activity(Resource):
     def get(self):
         return {'activity': 'Doing a Thing'}
     
+    @marshal_with(activity_response_fields)
     def post(self):
-        args = parse_args()
-        activity = {'name': args['name']}
+        activity = parse_args()
 
         # insert the activity
         result = db.insert_one('activity', activity) 
--- optimized_self/activity/post_parser.py
+++ optimized_self/activity/post_parser.py
@@ -1,8 +1,21 @@
 from flask_restful import reqparse
+from datetime import datetime
 
 # initialize the parser
 post_parser = reqparse.RequestParser()
-post_parser.add_argument('name')
+
+post_parser.add_argument(
+    name='name',
+    required=True,
+)
+post_parser.add_argument(
+    name='time_start',
+    default=datetime.utcnow()
+)
+post_parser.add_argument(
+    name='time_end',
+    default=datetime.utcnow()
+)
 
 def parse_args():
     return post_parser.parse_args()
